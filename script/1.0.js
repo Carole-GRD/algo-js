674c987c4872ffc85cb72ab46ce166cdd643f58c
Carole Gérard <gerard.carole78@gmail.com>
2023-09-08 11:31:55 +0200

    1.0.js

---
+++ script/1.0.js
@@ -0,0 +1,24 @@
+let num1 = 5;
+let num2 = 7;
+console.log('num1 = ' + num1 + ' et num2 = ' + num2);
+
+let add = `Addition : ${num1} + ${num2} = ${num1 + num2}`;
+console.log(add);
+
+let diff = `Difference : ${num1} - ${num2} = ${num1 - num2}`;
+console.log(diff);
+
+let mult = `Mutliplication : ${num1} * ${num2} = ${num1 * num2}`;
+console.log(mult);
+
+let div = `Division : ${num1} / ${num2} = ${num1 / num2}`;
+console.log(div);
+
+let modulo = `Modulo : ${num1} % ${num2} = ${num1 % num2}`;
+console.log(modulo);
+
+
+
+
+
+
