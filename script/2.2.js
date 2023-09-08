a730e52e7355f33b0237254e13c6b5a677138c01
Carole Gérard <gerard.carole78@gmail.com>
2023-09-08 16:17:19 +0200

    2.2.js - verification values, do...while, ternaire

--- script/2.2.js
+++ script/2.2.js
@@ -36,18 +36,60 @@
 
 // // Bonus 2: be polite in the error messages. (facultative)
 
-let min = prompt('Minimum number :');
-let max = prompt('Maximum number :');
+// let min = prompt('Minimum number :');
+// let max = prompt('Maximum number :');
 
-if ((min > max)) {
-    alert("Your 'min' value cannot be greater than 'max'. Please review the instructions.");
-}
-else {
-    let current = prompt('Current number :');
-    if ((current > min) && (current < max)) {
-        alert('Number between min and max');
+// if ((min > max)) {
+//     alert("Your 'min' value cannot be greater than 'max'. Please review the instructions.");
+// }
+// else {
+//     let current = prompt('Current number :');
+//     if ((current > min) && (current < max)) {
+//         alert('Number between min and max');
+//     }
+//     else {
+//         alert('Number NOT between min and max');
+//     }
+// }
+
+
+
+// ----------------
+
+do {
+    if (isNaN(min) || isNaN(max)) {
+        alert('Please enter valid numbers');
+        min = parseInt(prompt('Enter the minimum number:'));
+        max = parseInt(prompt('Enter the maximum number:'));
     }
-    else {
-        alert('Number NOT between min and max');
+    else if (min > max) {
+        alert("Your 'min' value cannot be greater than 'max'");
+        min = parseInt(prompt('Enter the minimum number:'));
+        max = parseInt(prompt('Enter the maximum number:'));
     }
+} 
+while (isNaN(min) || isNaN(max) || (min > max))
+
+
+do {
+    alert('Please enter valid number');
+    current = parseInt(prompt('Enter the current number:'));
 }
+while (isNaN(current))
+
+
+let text = 
+    (current >= min && current <= max) ? 
+    'Number is between min and max' : 
+    'Number is NOT between min and max';
+
+
+alert(text);
+
+
+
+
+
+
+
+
