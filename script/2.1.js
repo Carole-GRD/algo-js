66ecdb5acc5526ce92ffd531bf00ac39910458d2
Carole Gérard <gerard.carole78@gmail.com>
2023-09-08 15:23:07 +0200

    2.1.js - condition ternaire

--- script/2.1.js
+++ script/2.1.js
@@ -1,10 +1,8 @@
 // Ask the user to enter its age. If its age is at least 18 display "You are an adult". If it's not the case display "You are not yet an adult".
 
-let age  = prompt("What's your age ?");
 
-if (age < 18) {
-    alert('You are not yet an adult');
-}
-else {
-    alert('You are an adult');
-}
+let age  = parseInt(prompt("What's your age ?"));
+
+let majority = age < 18 ? 'You are not yet an adult' : 'You are an adult';
+
+alert(majority);
