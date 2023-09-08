db8fa10c671810068dd5bf31bfe59f82f8be053a
Carole Gérard <gerard.carole78@gmail.com>
2023-09-08 14:54:39 +0200

    2.3.js correction

--- script/2.3.js
+++ script/2.3.js
@@ -1,19 +1,19 @@
 // Display all even numbers between 1 and 100.
 
+// let i = 1;
+
+// while (i <= 100) {
+//     if (i % 2 === 0) {
+//         console.log(i);
+//     }
+//     i++;
+// }
 
 
 // Bonus: do two different versions. The first one should use while and make a loop that will execute 100 times. The second one should use for and make a loop that will execute 50 times.
 
 
-// let i = 1;
-
-// while(i <= 100) {
-//     console.log(i);
-//     i++;
-// }
-
-
-for (let i = 1; i < 51; i++) {
+for (let i = 2; i < 101; i += 2) {
     console.log(i);
 }
 
