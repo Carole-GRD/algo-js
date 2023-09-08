997780200170e26b32896f7c68cb67b80459d0cc
Carole Gérard <gerard.carole78@gmail.com>
2023-09-08 16:22:15 +0200

    2.4.js - condition ternaire

--- script/2.4.js
+++ script/2.4.js
@@ -1,10 +1,6 @@
 // Count from 1 to 100. For every even number display the result of their division by 2. For every odd number display the result of their multiplication by 3.
 
 for (let i = 1; i < 101; i++) {
-    if (i % 2 === 0) {
-        console.log(i/2);
-    }
-    else {
-        console.log(i*3);
-    }
+    let value = (i % 2 === 0) ? i/2 : i*3;
+    console.log(value);
 }
