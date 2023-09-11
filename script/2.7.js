372c7a815807f1d9e3ffa05514eed5edcbe05a07
Carole Gérard <gerard.carole78@gmail.com>
2023-09-11 09:15:50 +0200

    Create a table to store the numbers entered and display the calculation

--- script/2.7.js
+++ script/2.7.js
@@ -3,13 +3,41 @@
 // Example: If the user enters 3 for n then 1, 2 and 3, the program should display 6.
 
 
-let n = parseInt(prompt('How many numbers do you want to add up ?'));
-let sum = 0;
+// let n = parseInt(prompt('How many numbers do you want to add up ?'));
+// let sum = 0;
 
+// for (let i = 1; i <= n; i++) {
+//     sum += parseInt(prompt(`Number ${i} :`));
+//     console.log(i);
+//     console.log(sum);
+// }
+
+// alert(`Result : ${sum}`);
+
+
+
+
+// Création d'un tableau pour stocker les nombres entrés et afficher le calcul
+// -----------------------------------------------
+
+let n = parseInt(prompt('How many numbers do you want to add up ?'));
+
+// Create sum and array of numbers
+let sum = 0;
+let numbersTab = [];
+let calcul = '';
 for (let i = 1; i <= n; i++) {
-    sum += parseInt(prompt(`Number ${i} :`));
-    console.log(i);
-    console.log(sum);
+    // Number input
+    let value = parseInt(prompt(`Number ${i} :`));
+
+    // Add number to sum
+    sum += value;
+
+    // Add number to table
+    numbersTab = [...numbers, value];
+
+    // Create calculation display 
+    (i <= n) ? calcul += `${value} + ` : calcul += `${value} = `;
 }
 
-alert(`Result : ${sum}`);
+alert(calcul + sum);
