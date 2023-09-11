79d66b8eda434aec5e5e28c425b5db24f9ca1734
Carole Gérard <gerard.carole78@gmail.com>
2023-09-11 12:37:45 +0200

    4.3.js

--- script/4.3.js
+++ script/4.3.js
@@ -1,2 +1,37 @@
+// By reusing the function rand10() created in Exercise 2, write a function named multiRand(n) that returns an array of n numbers between 1 and 10. You should not modify anything in rand10() to achieve this.
+
+// Write a documentation for the multiRand(n) function.
+
+// Use that function to create a program that will ask the number of random numbers to generate then display that same number of random numbers.
 
 
+// --------------------------------------------------------
+
+/**
+ * Generates a random integer between 1 and 10 (inclusive).
+ * @returns {number} A random integer between 1 and 10 (inclusive).
+ */
+let rand10 = () => Math.ceil(Math.random() * 10);
+
+
+
+/**
+ * Generates an array of 'n' random integers between 1 and 10 (inclusive).
+ * @param {number} n - The number of random integers to generate.
+ * @returns {number[]} An array of 'n' random integers between 1 and 10 (inclusive).
+ */
+let multiRand = (n) => {
+    let arr = [];
+    let i = 1;
+    while (i <= n) {
+        let num = rand10();
+        arr = [...arr, num];
+        i++;
+    }
+    return arr;
+}
+
+
+let elements = parseInt(prompt('Combien de nombres dans le tableau ?'));
+alert(multiRand(elements));
+// console.log(multiRand(elements));
