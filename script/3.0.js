e164356d6980605e2bea5438d2d71cf2ced402e9
Carole Gérard <gerard.carole78@gmail.com>
2023-09-11 09:28:59 +0200

    3.0.js

---
+++ script/3.0.js
@@ -0,0 +1,34 @@
+// Create an array that contains every learner from the class (you can find the complete list on the Discord server). Display each name in the console using a for ... of ... loop.
+
+let learners = [
+    'Alexandre Vandewiele', 
+    'Antoine Lansman',
+    'Bastien Venturi',
+    'Carole Gérard',
+    'Dorian Vanden Eynde',
+    'Elisabeth Leyder',
+    'Elodie Ali',
+    'Justin Michel',
+    'Justine Frigo',
+    'Justine Leleu',
+    'Kimi Lefort',
+    'Layla',
+    'Lidwine Careme',
+    'Lucas Beauloi',
+    'Marie Tara',
+    'Mathias Barbier',
+    'okly2023',
+    'Pierre Marien',
+    'Robin Piot',
+    'Rosalie Boulard',
+    'Stephane Comblez',
+    'Tim Desmet',
+    'Toms',
+    'Valentin Lefort',
+    'Vens Alexandre',
+    'Virginie Dourson'
+];
+
+for (let learner of learners) {
+    console.log(learner);
+}
